af794433065441e474e024f724f7ea80c814d432
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2020-01-29 04:29:25 +0000

    Add config.py to handle DB-related config
    config.py is used to load the DB URI from the appropriate
    environment variable. Ideally, the underlying database
    can be abstracted away from the backend service to allow
    for easier testing on local machines.

--- kronostore.py
+++ kronostore.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello World<h1>"
+    return "<h1 style='color:blue'>Did I break it<h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
